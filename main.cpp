f8a688f8fdd1e531492bad2aa44b905c4eb58440
Vipin-1999 <vipinrbharadwaj@gmail.com>
2019-07-25 09:14:35 +0530

    Final Commit

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
-#include "telephone.h"
+#include "mobile.h"
 
 using namespace std;
 
 int main()      {
-    Telephone t;
+    Mobile m;
     std::string number;
     for(auto i = 0; i < 10; i++)    {//1 for valid and 0 for invalid
-        number = t.Random((i+1)%2, "91", "80");
-        cout << number << " : "<< t.Validate(number) << endl;
+        number = m.Random((i+1)%2);
+        cout << number << " : "<< m.Validate(number) << endl;
     }
     return 0;
 }
