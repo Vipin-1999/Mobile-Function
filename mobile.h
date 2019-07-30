506a1f10b53a985fbdc5df2669ee60aae9acb3d9
Vipin-1999 <vipinrbharadwaj@gmail.com>
2019-07-30 12:45:27 +0530

    Final Commit

--- mobile.h
+++ mobile.h
@@ -6,7 +6,7 @@
 #include<regex>
 typedef unsigned long size_t;
 
-// #include "nbind/nbind.h"
+#include "nbind/nbind.h"
 
 class Mobile{
     public:
@@ -19,3 +19,13 @@ class Mobile{
 };
 
 #endif
+
+NBIND_CLASS(Mobile){
+    construct<>();
+    multimethod(Validate, args(std::string), "Validate");
+    multimethod(Validate, args(std::string, std::string), "ValidateWithCode");
+    multimethod(Random, args(bool), "Random");
+    multimethod(Random, args(bool, std::string), "RandomWithCode");
+    method(Generate);
+    method(Generate_Invalid);
+}
